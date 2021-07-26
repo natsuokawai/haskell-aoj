5fc34344bf88bb8e0d4e4e9365744635576946ed
natsuokawai <kawai.natsuo@gmail.com>
2021-07-26 22:36:40 +0900

    TP1_5_B

---
+++ src/ITP1_5_B.hs
@@ -0,0 +1,23 @@
+import Data.Maybe
+import qualified Data.ByteString.Char8 as BS
+
+readInt = fst . fromJust . BS.readInt
+readIntList = map readInt . BS.words
+readIntListLines = map readIntList . BS.lines
+
+printFrame [h, w] idx
+    | w == 0 = return ()
+    | idx == h = putStrLn ""
+    | otherwise = do
+        case idx of i | i == 0 || i == h - 1 -> putStrLn $ BS.unpack $ BS.replicate w '#'
+                      | otherwise -> putStrLn $ "#" ++ (BS.unpack $ BS.replicate (w - 2) '.') ++ "#"
+        printFrame [h, w] (idx + 1)
+
+printFrameN [] = return ()
+printFrameN (x:xs) = do
+    printFrame x 0
+    printFrameN xs
+
+main = do
+    lines <- fmap readIntListLines BS.getContents
+    printFrameN lines
